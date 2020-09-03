9f0d8287c40d889e92df544aaa01e9a5fc327eba
Pedro Piñera <pepibumur@gmail.com>
2020-09-03 19:15:11 +0200

    Implement and test AsyncQueuePersistor

--- Sources/tuist/main.swift
+++ Sources/tuist/main.swift
@@ -1,11 +1,15 @@
 import Foundation
 import enum TSCBasic.ProcessEnv
-import TuistSupport
 import enum TuistSupport.LogOutput
+import TuistSupport
+import TuistAsyncQueue
 
 if CommandLine.arguments.contains("--verbose") { try? ProcessEnv.setVar(Constants.EnvironmentVariables.verbose, value: "true") }
 
 LogOutput.bootstrap()
 
 import TuistKit
-TuistCommand.main()
+
+try AsyncQueue.run {
+    TuistCommand.main()
+}
